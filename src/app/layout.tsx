574ddcf5ec012e6383fa0de43b537a661811dfc3
Leonel Rivas <leonelrivas.rl@gmail.com>
2025-09-29 13:26:58 -0300

    added new font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,76 @@
 import type {Metadata} from "next";
 
 import Link from "next/link";
+import localFont from "next/font/local";
 
 import {Button} from "@/components/ui/button";
 import "./globals.css";
+
+const styreneB = localFont({
+  src: [
+    {
+      path: "../../public/fonts/styrene/StyreneB-Thin-Trial-BF63f6cc84a4246.otf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/styrene/StyreneB-ThinItalic-Trial-BF63f6cbe8d3333.otf",
+      weight: "100",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/styrene/StyreneB-Light-Trial-BF63f6cbe64b47b.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/styrene/StyreneB-LightItalic-Trial-BF63f6cbe9d67d1.otf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/styrene/StyreneB-Regular-Trial-BF63f6cbe9db1d5.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/styrene/StyreneB-RegularItalic-Trial-BF63f6cbe5c1942.otf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/styrene/StyreneB-Medium-Trial-BF63f6cc85760c2.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/styrene/StyreneB-MediumItalic-Trial-BF63f6cc84a421a.otf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/styrene/StyreneB-Bold-Trial-BF63f6cbe9f13bb.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/styrene/StyreneB-BoldItalic-Trial-BF63f6cbe6863e6.otf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/styrene/StyreneB-Black-Trial-BF63f6cbe3dc69b.otf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/styrene/StyreneB-BlackItalic-Trial-BF63f6cbe2d985b.otf",
+      weight: "900",
+      style: "italic",
+    },
+  ],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Anfibios de Ecuador - SapoPedia",
@@ -14,7 +81,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="es">
-      <body className="bg-background min-h-screen font-sans antialiased">
+      <body className={`${styreneB.className} bg-background min-h-screen font-sans antialiased`}>
         <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 border-b backdrop-blur">
           <div className="container mx-auto px-4 py-4">
             <div className="flex items-center justify-between">
